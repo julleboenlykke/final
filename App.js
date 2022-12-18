a629b2a2835ad8abc2fdc9778fa1016fc0edba17
julleboenlykke <83392045+julleboenlykke@users.noreply.github.com>
2022-12-18 11:48:58 +0100

    new

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {AuthStack, ProfileTab} from "./components/navigators/Navigators";
+import {NavigationContainer} from "@react-navigation/native";
+import firebase from "firebase/compat";
+import React, { useEffect, useState} from "react";
+import {AppContext, StateProvider} from "./components/AppContext";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYyGvmmFWDHyL0DnWuXhC1_Gx25EXv41M",
+  authDomain: "eksamen-c7a2c.firebaseapp.com",
+  databaseURL: "https://eksamen-c7a2c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "eksamen-c7a2c",
+  storageBucket: "eksamen-c7a2c.appspot.com",
+  messagingSenderId: "580927712033",
+  appId: "1:580927712033:web:dfc9e66a62f7fdcd9c2c60"
+};
+
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  //Her oprettes bruger state variblen
+  //const {globalUser, setGlobalUser } = useContext(AppContext);
+  const [user, setUser] = useState({ loggedIn: false });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  //Koden sikrer at kun én Firebase initieres under brug af appen.
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  //onAuthstatechanged er en prædefineret metode, forsynet af firebase, som konstant observerer brugerens status (logget ind vs logget ud)
+//Pba. brugerens status foretages et callback i form af setUSer metoden, som håndterer user-state variablens status.
+  function onAuthStateChange(callback) {
+    return firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        callback({loggedIn: true, user: user});
+      } else {
+        callback({loggedIn: false});
+      }
+    });
+  }
+
+  //Heri aktiverer vi vores listener i form af onAuthStateChanged, så vi dynamisk observerer om brugeren er aktiv eller ej.
+  useEffect(() => {
+    const unsubscribe = onAuthStateChange(setUser);
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  //Her oprettes gæstekomponentsindhold, der udgøres af sign-up og login siderne
+  const ValidateUser = () => {
+    return (
+        <NavigationContainer>
+          <AuthStack/>
+        </NavigationContainer>
+    );
+  }
+  const Profile = () => {
+    return (
+        <NavigationContainer><ProfileTab/></NavigationContainer>
+    );
+  }
+  return (user.loggedIn ? <StateProvider><Profile/></StateProvider> :<StateProvider><ValidateUser/></StateProvider>) ;
+
+}
